836a11378dbd9d493c58fac6a6951d2ce0d5cb9a
m95niri <rickard.nilsson.ldk@me.com>
2015-10-20 20:57:31 +0200

    updating text to test git functions

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,3 @@
-package helloworld;
 
 /******************************************************************************
  *  Compilation:  javac HelloWorld.java
@@ -22,7 +21,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World testing GIT");
     }
 
 }
